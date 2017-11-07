2a38192ea6e900c354f13141708376e23f20c5e4
Wojciech Makurat <wojtek@infoshareacademy.com>
2017-11-07 20:32:44 +0100

    pliki dzien 2

---
+++ dzien2/zmienne.py
@@ -0,0 +1,16 @@
+imie = "Joanna"
+Imie = "Anna"
+nazwisko = "Kowalska"
+wiek = 34
+
+# print(imie)
+# print(imie[3:5])
+# print(3 + 7)
+# print(3 == 5)
+# print(imie.lower())
+# print(imie.count('a'))
+
+print(imie + ' ' + nazwisko + 'ma' + str(wiek) + 'lata.')
+
+print(f"{imie} {nazwisko} ma {wiek / 2} lata.")
+print("{0} {1} ma {2} lata".format(imie, nazwisko, wiek))
